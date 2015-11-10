7e8c35bd520323a753644765d2a2a337c52b8829
John  Touba <john.touba@gmail.com>
2015-11-10 13:11:29 +0100

    Change method to import js file

---
+++ server/static/js/password.js
@@ -0,0 +1,11 @@
+ $(function() {
+        $('#submit').bind('click', function() {
+          $.getJSON($SCRIPT_ROOT + '/generate_pass', {
+            a: $('input[value="special_char"]').val(),
+            b: $('input[value="number"]').val()
+          }, function(data) {
+            $("#password_gen").text(data.password_to_return);
+          });
+          return false;
+        });
+      });
